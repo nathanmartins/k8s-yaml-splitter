a3ae4783989b095e7cce822768da9751255c1cac
Nathan M <nathan.eua@gmail.com>
2025-09-03 14:12:08 +0300

    chore: add .golangci.yml configuration and update Go modules

--- cmd/root.go
+++ cmd/root.go
@@ -2,25 +2,28 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"io"
-	"log"
+	"log/slog"
 	"os"
-	"sigs.k8s.io/kustomize/kyaml/kio"
 	"strings"
+
+	"github.com/spf13/cobra"
+	"sigs.k8s.io/kustomize/kyaml/kio"
 )
 
-// rootCmd represents the base command when called without any subcommands
+// rootCmd represents the base command when called without any subcommands.
 var rootCmd = &cobra.Command{
 	Use:   "yaml-split",
 	Short: "this command will transform a list of yaml documents into individual documents",
 	Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
-	Run: func(cmd *cobra.Command, args []string) {
+	Run: func(_ *cobra.Command, args []string) {
 
 		var firstFileBytes []byte
 		var err error
 
-		if len(args) < 2 {
+		logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+
+		if len(args) < 2 { //nolint:mnd // not magic number
 			firstFileBytes, err = io.ReadAll(os.Stdin)
 		} else {
 			fName := args[0]
@@ -28,12 +31,14 @@ var rootCmd = &cobra.Command{
 		}
 
 		if err != nil {
-			log.Fatal(err)
+			logger.Error("error reading file", "err", err)
+			os.Exit(-1)
 		}
 
 		firstFile, err := kio.FromBytes(firstFileBytes)
 		if err != nil {
-			log.Fatalln(err)
+			logger.Error("error reading file", "err", err)
+			os.Exit(-1)
 		}
 
 		result := args[0]
@@ -42,18 +47,23 @@ var rootCmd = &cobra.Command{
 			result = args[1]
 		}
 
-		err = os.MkdirAll(result, os.ModePerm)
+		err = os.MkdirAll(result, 0750)
 		if err != nil {
-			log.Fatal(err)
+			logger.Error("error creating directory", "err", err)
+			os.Exit(-1)
 		}
 
 		for _, node := range firstFile {
 
 			fileName := fmt.Sprintf("%s-%s.yaml", strings.ToLower(node.GetKind()), strings.ToLower(node.GetName()))
 			fileName = strings.ReplaceAll(fileName, ":", "-")
-			fmt.Printf("processing: %s\n", fileName)
+			logger.Info("processing", "fileName", fileName)
 
-			OverWriteToFile(fmt.Sprintf("%s/%s", result, fileName), node.MustString())
+			OverWriteToFile(
+				fmt.Sprintf("%s/%s", result, fileName),
+				node.MustString(),
+				logger,
+			)
 		}
 
 	},
@@ -80,25 +90,28 @@ func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// ReadFileBytes will read the entire contents of a file and return it as an array of bytes
+// ReadFileBytes will read the entire contents of a file and return it as an array of bytes.
 func ReadFileBytes(filePath string) ([]byte, error) {
 	b, err := os.ReadFile(filePath) // just pass the file name
 	return b, err
 }
 
-// OverWriteToFile this will truncate the file and write over any contents
-func OverWriteToFile(filePath string, payload string) {
+// OverWriteToFile this will truncate the file and write over any contents.
+func OverWriteToFile(filePath string, payload string, logger *slog.Logger) {
 	var f *os.File
 
-	// If the file doesn't exist, create it, or append to the file
-	f, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	// If the file doesn't exist, create it, or append to the file.
+	f, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("error opening file", "err", err)
+		os.Exit(-1)
 	}
-	if _, err = f.Write([]byte(payload)); err != nil {
-		log.Fatal(err)
+	if _, err = f.WriteString(payload); err != nil {
+		logger.Error("error writing to file", "err", err)
+		os.Exit(-1)
 	}
 	if err = f.Close(); err != nil {
-		log.Fatal(err)
+		logger.Error("error closing file", "err", err)
+		os.Exit(-1)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,29 +1,29 @@
 module go.nathanmartins.sh/k8s-yaml-splitter
 
-go 1.23.5
+go 1.25
 
 require (
-	github.com/spf13/cobra v1.9.1
+	github.com/spf13/cobra v1.10.1
 	sigs.k8s.io/kustomize/kyaml v0.20.1
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
-	github.com/go-errors/errors v1.5.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-errors/errors v1.4.2 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
-	github.com/go-openapi/jsonreference v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/google/gnostic-models v0.6.9 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/mailru/easyjson v0.9.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
-	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/spf13/pflag v1.0.9 // indirect
 	github.com/xlab/treeprint v1.2.0 // indirect
 	go.yaml.in/yaml/v2 v2.4.2 // indirect
 	go.yaml.in/yaml/v3 v3.0.3 // indirect
 	golang.org/x/sys v0.29.0 // indirect
-	google.golang.org/protobuf v1.36.3 // indirect
+	google.golang.org/protobuf v1.36.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20241212222426-2c72e554b1e7 // indirect
 )
